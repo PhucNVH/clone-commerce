cc9ccba2542508ce08de8e75f94db0dad9ce2b8a
Hoang-Phuc Nguyen-Vu <phucnvh@dwarvesf.com>
2020-07-17 09:35:27 +0700

    feat: add header

--- src/components/header.js
+++ src/components/header.js
@@ -1,76 +1,64 @@
-import { graphql, useStaticQuery, Link } from "gatsby";
-import React, { useState } from "react";
+import { Link } from "gatsby";
+import React from "react";
 
 function Header() {
-  const [isExpanded, toggleExpansion] = useState(false);
-  const { site } = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
-    <header className="bg-teal-700">
-      <div className="flex flex-wrap items-center justify-between max-w-4xl p-4 mx-auto md:p-8">
-        <Link to="/">
-          <h1 className="flex items-center text-white no-underline">
-            <svg
-              className="w-8 h-8 mr-2 fill-current"
-              height="54"
-              viewBox="0 0 54 54"
-              width="54"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
-            <span className="text-xl font-bold tracking-tight">
-              {site.siteMetadata.title}
-            </span>
-          </h1>
-        </Link>
+    <header className="bg-white h-32 flex justify-center">
+      <div className="flex flex-wrap items-center justify-between mx-20 w-full">
+        <div className="w-4/6 flex h-full items-center">
+          <Link to="/" className="">
+            <img
+              src={
+                "https://wwwcdn.bigcommerce.com/www1.bigcommerce.com/assets/logos/bc-logo-dark.svg"
+              }
+            />
+          </Link>
 
+          <nav className={`md:flex md:items-center h-full justify-start`}>
+            {[
+              {
+                route: `/`,
+                title: `Solutions`,
+              },
+              {
+                route: `/`,
+                title: `Products`,
+              },
+              {
+                route: `/`,
+                title: `Customers`,
+              },
+              {
+                route: `/`,
+                title: `Partners`,
+              },
+              {
+                route: `/`,
+                title: `Resources`,
+              },
+            ].map((link) => (
+              <button
+                key={link.title.toString()}
+                className="header-nav-item block text-black no-underline md:inline-block md:mt-0 hover:bg-gray-300 h-full px-4 text-sm"
+              >
+                {link.title}
+                <div className="hidden">
+                  <ul>
+                    <li>ABC</li>
+                    <li>DEF</li>
+                    <li>GHK</li>
+                  </ul>
+                </div>
+              </button>
+            ))}
+          </nav>
+        </div>
         <button
-          className="flex items-center block px-3 py-2 text-white border border-white rounded md:hidden"
-          onClick={() => toggleExpansion(!isExpanded)}
+          className="bg-blue-800 text-white font-bold rounded px-8 py-2 uppercase"
+          style={{ backgroundColor: "#0d52ff" }}
         >
-          <svg
-            className="w-3 h-3 fill-current"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
+          Request Demo
         </button>
-
-        <nav
-          className={`${
-            isExpanded ? `block` : `hidden`
-          } md:block md:flex md:items-center w-full md:w-auto`}
-        >
-          {[
-            {
-              route: `/about`,
-              title: `About`,
-            },
-            {
-              route: `/contact`,
-              title: `Contact`,
-            },
-          ].map((link) => (
-            <Link
-              className="block mt-4 text-white no-underline md:inline-block md:mt-0 md:ml-6"
-              key={link.title}
-              to={link.route}
-            >
-              {link.title}
-            </Link>
-          ))}
-        </nav>
       </div>
     </header>
   );
